ee0d54ef2d96e6a6f7e21fcc670e5b4b0b7b4197
justt277 <AIJUSTICE@proton.me>
2025-07-03 21:20:14 +0200

    initial commit

---
+++ Myreact-app1/src/Message.tsx
@@ -0,0 +1,10 @@
+
+ //PascalCasing
+function Message(){
+     const name = '';
+     if (name)
+    return <h1>Legends Never Die999 {name}</h1>;
+     return <h1>hello world</h1>
+}
+
+export default Message;
