c4c82cab552f5ac2b9861ae9c3241cb2c37066a5
justt277 <AIJUSTICE@proton.me>
2025-09-23 09:44:13 +0300

    my first commit

---
+++ ReactPro/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Greetings from './greetings'
+
+const App = () => {
+  return (
+    <div>
+      <Greetings/>
+    </div>
+  )
+}
+
+export default App;
---
+++ ReactPro/src/greetings.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Greetings (){
+    return (
+        <div>
+            <h1 style = {{fontStyle:'revert-layer', fontSize:'20px' , color:'blue'}}>hello,justice</h1>
+            <p style={{fontFamily:'monospace' , color:'blue'}}>wellcome n' feel free to use ReactJs</p>
+        </div>
+    )
+}
